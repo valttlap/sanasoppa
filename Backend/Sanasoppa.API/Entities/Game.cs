d56a638da3531afa414bb497ac86d40361553974
Valtteri Lappalainen <valtteri.lappalainen@gmail.com>
2023-03-05 01:36:38 +0200

    Fixed wrong namings

--- Backend/Sanasoppa.API/Entities/Game.cs
+++ Backend/Sanasoppa.API/Entities/Game.cs
@@ -12,7 +12,7 @@
         public bool HasStarted { get; set; }
         public ICollection<Player> Players { get; set; }
         public int GameState { get; set; }
-        public GameState CurrentGameState { get; set; }
+        public GameState CurrentGameState { get; set; } = default!;
         public int? CurrentRoundId { get; set; }
         public Round? CurrentRound { get; set; }
         public ICollection<Round> Rounds { get; set; }
