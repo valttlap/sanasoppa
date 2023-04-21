75678dc4cef726c0cf9ef48ca84108ff8ca2f5c1
Valtteri Lappalainen <valtteri.lappalainen@gmail.com>
2023-04-22 01:27:18 +0300

    Fixed deepsource issues

--- Backend/Sanasoppa.API/DTOs/ForgotPasswordDto.cs
+++ Backend/Sanasoppa.API/DTOs/ForgotPasswordDto.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Sanasoppa.API.DTOs
+namespace Sanasoppa.API.DTOs;
+
+public class ForgotPasswordDto
 {
-    public class ForgotPasswordDto
-    {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; } = default!;
-        [Required]
-        public string ReCaptchaResponse { get; set; } = default!;
-    }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = default!;
+    [Required]
+    public string ReCaptchaResponse { get; set; } = default!;
 }
